6cf24060ae31a430f874c0a7f7bdf7168d54ebd8
Sumire Honda <pu.sumirehonda@gmail.com>
2023-07-22 13:59:50 +0000

    add {} for each document and append doc to a list

--- data/xml_to_json.py
+++ data/xml_to_json.py
@@ -2,6 +2,7 @@ import pandas as pd
 from bs4 import BeautifulSoup
 import numpy as np
 import json
+import jsonlines
 
 def build_dictionary_from_xml(xml_filepath_list, lang):
     """
@@ -18,6 +19,7 @@ def build_dictionary_from_xml(xml_filepath_list, lang):
     docs_dict = {}
     for docs in files_list:
         for doc in docs:
+        
             #docid = int(doc['docid'])
             talkid = int(doc.find('talkid').get_text())
             seg_elements = doc.find_all('seg')
@@ -38,18 +40,22 @@ def get_shared_talkids(lang_dicts):
 def combine_langs_into_one_dict(lang_dicts, shared_talk_ids):
     
     _multilingual_talks_dict = {}
+    doc_dict_list = []
 
     for talk_id in shared_talk_ids:
         talk_dict = {}
         talk_dict[talk_id] = {}
-        
+    
         for lang_dict in lang_dicts:
             talk_dict[talk_id].update(lang_dict[talk_id])
-        
-        _multilingual_talks_dict.update(talk_dict)
-                
-    return _multilingual_talks_dict
+        print (talk_dict)
+        doc_dict = {}
+        doc_dict["doc"] = talk_dict
 
+        _multilingual_talks_dict.update(doc_dict)
+        doc_dict_list.append({"doc": talk_dict})
+    
+    return doc_dict_list
 
 # Create Parallel json dataset
 def generate_parallel_json(parallel_lang_list, data_dir):
@@ -71,9 +77,11 @@ def generate_parallel_json(parallel_lang_list, data_dir):
             docs_dict = build_dictionary_from_xml(xml_filepath_list, lang)
             parallel_lang_dicts.append(docs_dict)
         shared_talk_ids = get_shared_talkids(lang_dicts=parallel_lang_dicts)
-        parallel_talks_dict = combine_langs_into_one_dict(lang_dicts = parallel_lang_dicts, shared_talk_ids=shared_talk_ids)
-
+        parallel_talks_dict = combine_langs_into_one_dict(lang_dicts = parallel_lang_dicts, shared_talk_ids=shared_talk_ids)    
+        
         with open(data_dir+f"/ted_{parallel[0]}-{parallel[1]}", "w", encoding="utf-8") as outfile:
             json.dump(parallel_talks_dict, outfile, indent=4, ensure_ascii=False)
+
+
 
 
