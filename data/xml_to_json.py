65f4cf1e0c5e1b17a318716fa7bc85174571badf
Sumire Honda <pu.sumirehonda@gmail.com>
2023-07-22 09:02:00 +0000

    cleaned project structure

--- data/xml_to_json.py
+++ data/xml_to_json.py
@@ -1,4 +1,3 @@
-from datasets import load_dataset
 import pandas as pd
 from bs4 import BeautifulSoup
 import numpy as np
@@ -29,15 +28,6 @@ def build_dictionary_from_xml(xml_filepath_list, lang):
     
     return docs_dict
 
-en_xml_filepath_list = ["/home/sumire/thesis/LLM_Contextual_Prompt_MT/data/iwslt_hf/ja-en/IWSLT17.TED.dev2010.ja-en.en.xml",
-                        "/home/sumire/thesis/LLM_Contextual_Prompt_MT/data/iwslt_hf/ja-en/IWSLT17.TED.tst2010.ja-en.en.xml",
-                        "/home/sumire/thesis/LLM_Contextual_Prompt_MT/data/iwslt_hf/ja-en/IWSLT17.TED.tst2011.ja-en.en.xml",
-                        "/home/sumire/thesis/LLM_Contextual_Prompt_MT/data/iwslt_hf/ja-en/IWSLT17.TED.tst2012.ja-en.en.xml",
-                        "/home/sumire/thesis/LLM_Contextual_Prompt_MT/data/iwslt_hf/ja-en/IWSLT17.TED.tst2013.ja-en.en.xml",
-                        "/home/sumire/thesis/LLM_Contextual_Prompt_MT/data/iwslt_hf/ja-en/IWSLT17.TED.tst2014.ja-en.en.xml",
-                        "/home/sumire/thesis/LLM_Contextual_Prompt_MT/data/iwslt_hf/ja-en/IWSLT17.TED.tst2015.ja-en.en.xml",
-                       ]
-
 def get_shared_talkids(lang_dicts):
     shared_keys = set(lang_dicts[0].keys())
     for i in lang_dicts[1:]:
@@ -59,4 +49,31 @@ def combine_langs_into_one_dict(lang_dicts, shared_talk_ids):
         _multilingual_talks_dict.update(talk_dict)
                 
     return _multilingual_talks_dict
-                
+
+
+# Create Parallel json dataset
+def generate_parallel_json(parallel_lang_list, data_dir):
+
+    for parallel in parallel_lang_list:
+        parallel_lang_dicts = []
+        tgt_lang = parallel[1]
+        
+        for lang in parallel:
+            xml_filepath_list = [
+                data_dir+f"{tgt_lang}-en/IWSLT17.TED.dev2010.{tgt_lang}-en.{lang}.xml",
+                data_dir+f"{tgt_lang}-en/IWSLT17.TED.tst2010.{tgt_lang}-en.{lang}.xml",
+                data_dir+f"{tgt_lang}-en/IWSLT17.TED.tst2011.{tgt_lang}-en.{lang}.xml",
+                data_dir+f"{tgt_lang}-en/IWSLT17.TED.tst2012.{tgt_lang}-en.{lang}.xml",
+                data_dir+f"{tgt_lang}-en/IWSLT17.TED.tst2013.{tgt_lang}-en.{lang}.xml",
+                data_dir+f"{tgt_lang}-en/IWSLT17.TED.tst2014.{tgt_lang}-en.{lang}.xml",
+                data_dir+f"{tgt_lang}-en/IWSLT17.TED.tst2015.{tgt_lang}-en.{lang}.xml",
+                            ]
+            docs_dict = build_dictionary_from_xml(xml_filepath_list, lang)
+            parallel_lang_dicts.append(docs_dict)
+        shared_talk_ids = get_shared_talkids(lang_dicts=parallel_lang_dicts)
+        parallel_talks_dict = combine_langs_into_one_dict(lang_dicts = parallel_lang_dicts, shared_talk_ids=shared_talk_ids)
+
+        with open(data_dir+f"/ted_{parallel[0]}-{parallel[1]}", "w", encoding="utf-8") as outfile:
+            json.dump(parallel_talks_dict, outfile, indent=4, ensure_ascii=False)
+
+
--- src/main.py
+++ src/main.py
@@ -1,33 +1,11 @@
-from datasets import load_dataset
-import pandas as pd
-from bs4 import BeautifulSoup
-import numpy as np
-import json
-from combine_xml_data import build_dictionary_from_xml, get_shared_talkids, combine_langs_into_one_dict
+from data.xml_to_json import generate_parallel_json
 
-
-# Read XML Dataset 
-parallel_lang_list = [["en", "ja"],["en", "ar"], ["en",  "fr"], ["en", "de"], ["en", "ko"], ["en", "zh"]]
-rel_path = "/home/sumire/thesis/LLM_Contextual_Prompt_MT/data/iwslt_hf/"
-
-# Create Parallel json dataset
-for parallel in parallel_lang_list:
-    parallel_lang_dicts = []
-    tgt_lang = parallel[1]
+def main():
+    # Read XML Dataset 
+    parallel_lang_list = [["en", "ja"],["en", "ar"], ["en",  "fr"], ["en", "de"], ["en", "ko"], ["en", "zh"]]
+    data_dir = "/home/sumire/thesis/LLM_Contextual_Prompt_MT/data/iwslt_hf/"
+    generate_parallel_json(parallel_lang_list, data_dir )
     
-    for lang in parallel:
-        xml_filepath_list = [
-            rel_path+f"{tgt_lang}-en/IWSLT17.TED.dev2010.{tgt_lang}-en.{lang}.xml",
-            rel_path+f"{tgt_lang}-en/IWSLT17.TED.tst2010.{tgt_lang}-en.{lang}.xml",
-            rel_path+f"{tgt_lang}-en/IWSLT17.TED.tst2011.{tgt_lang}-en.{lang}.xml",
-            rel_path+f"{tgt_lang}-en/IWSLT17.TED.tst2012.{tgt_lang}-en.{lang}.xml",
-            rel_path+f"{tgt_lang}-en/IWSLT17.TED.tst2013.{tgt_lang}-en.{lang}.xml",
-            rel_path+f"{tgt_lang}-en/IWSLT17.TED.tst2014.{tgt_lang}-en.{lang}.xml",
-            rel_path+f"{tgt_lang}-en/IWSLT17.TED.tst2015.{tgt_lang}-en.{lang}.xml",
-                           ]
-        docs_dict = build_dictionary_from_xml(xml_filepath_list, lang)
-        parallel_lang_dicts.append(docs_dict)
-    shared_talk_ids = get_shared_talkids(lang_dicts=parallel_lang_dicts)
-    parallel_talks_dict = combine_langs_into_one_dict(lang_dicts = parallel_lang_dicts, shared_talk_ids=shared_talk_ids)
-    with open(f"/home/sumire/thesis/LLM_Contextual_Prompt_MT/data/iwslt_hf/ted_{parallel[0]}-{parallel[1]}", "w", encoding="utf-8") as outfile:
-        json.dump(parallel_talks_dict, outfile, indent=4, ensure_ascii=False)
+if __name__ == "__main__":
+    main()
+
