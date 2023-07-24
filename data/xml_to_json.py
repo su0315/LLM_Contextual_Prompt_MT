396a7c5686466fcc7c9c4b4502c22e7523887e55
Sumire Honda <pu.sumirehonda@gmail.com>
2023-07-24 18:35:16 +0000

    extract only shared talks among languages

--- data/xml_to_json.py
+++ data/xml_to_json.py
@@ -48,7 +48,6 @@ def combine_langs_into_one_dict(lang_dicts, shared_talk_ids):
     
         for lang_dict in lang_dicts:
             talk_dict.update(lang_dict[talk_id])
-        print (talk_dict)
         doc_dict = {}
         doc_dict["talk_id"] = talk_id
         doc_dict["doc"] = talk_dict
@@ -79,10 +78,51 @@ def generate_parallel_json(parallel_lang_list, data_dir):
             parallel_lang_dicts.append(docs_dict)
         shared_talk_ids = get_shared_talkids(lang_dicts=parallel_lang_dicts)
         parallel_talks_dict = combine_langs_into_one_dict(lang_dicts = parallel_lang_dicts, shared_talk_ids=shared_talk_ids)    
-        
+
         with open(data_dir+f"/ted_{parallel[0]}-{parallel[1]}", "w", encoding="utf-8") as outfile:
             json.dump(parallel_talks_dict, outfile, indent=4, ensure_ascii=False)
 
+    # Drop instances of ja/fr/ko/ar/zh not existing in de
+    df_ja = pd.read_json(f"/home/sumire/thesis/LLM_Contextual_Prompt_MT/data/iwslt_hf/ted_en-ja")
+    df_zh = pd.read_json(f"/home/sumire/thesis/LLM_Contextual_Prompt_MT/data/iwslt_hf/ted_en-zh")
+    df_ko = pd.read_json(f"/home/sumire/thesis/LLM_Contextual_Prompt_MT/data/iwslt_hf/ted_en-ko")
+    df_ar = pd.read_json(f"/home/sumire/thesis/LLM_Contextual_Prompt_MT/data/iwslt_hf/ted_en-ar")
+    df_de = pd.read_json(f"/home/sumire/thesis/LLM_Contextual_Prompt_MT/data/iwslt_hf/ted_en-de")
+    df_fr = pd.read_json(f"/home/sumire/thesis/LLM_Contextual_Prompt_MT/data/iwslt_hf/ted_en-fr")
+    
+    lang_df_list = [df_ja, df_fr, df_ko, df_ar, df_zh] #  "de"
+    dropped_df_list = []
 
+    for df in lang_df_list:
+        dropped_rows = []
 
+        for talk_id in df["talk_id"].to_numpy():
+            if talk_id not in df_de["talk_id"].to_numpy():
+                dr_row = df[df["talk_id"]==talk_id].index.item()
+                dropped_rows.append(dr_row)
 
+        dropped_df = df.drop(dropped_rows)
+        dropped_df.reset_index(inplace=True, drop=True)
+        dropped_df_list.append(dropped_df)
+
+    # Drop instance of de not existing in others
+    dropped_rows = []
+
+    for talk_id in df_de["talk_id"].to_numpy():
+        if talk_id not in df_fr["talk_id"].to_numpy():
+            dr_row = df_de[df_de["talk_id"]==talk_id].index.item()
+            dropped_rows.append(dr_row)
+
+    dropped_df_de = df_de.drop(dropped_rows)
+    dropped_df_de.reset_index(inplace=True, drop=True)
+    dropped_df_list.append(dropped_df_de)
+    print (dropped_df_list)
+
+    lang_list = ["ja", "fr", "ko", "ar","zh","de"] 
+
+    for lang, dropped_df in zip (lang_list, dropped_df_list):
+        json_object = dropped_df.to_json(force_ascii=False, orient="records", indent = 4)
+        with open(f"/home/sumire/thesis/LLM_Contextual_Prompt_MT/data/iwslt_hf/ted_en-{lang}", "w", encoding='utf-8') as outfile:
+            outfile.write(json_object)
+
+    # Now all of the data has 92 documents 
