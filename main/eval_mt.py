92037f7771465c8696afee13243c786966061ae8
Sumire Honda <pu.sumirehonda@gmail.com>
2023-07-29 22:14:21 +0000

    model.generate for xglm

--- main/eval_mt.py
+++ main/eval_mt.py
@@ -27,19 +27,69 @@ training_args = Seq2SeqTrainingArguments(
     greater_is_better = True,
     #save_strategy = "steps",
     eval_delay = 0.0,
-    eval_accumulation_steps = 20
+    eval_accumulation_steps = 20,
+    label_names = ["labels"]
     
 )
 
 def main():
+
+    tgt_lang = "ja"
+    data_path = "/home/sumire/thesis/LLM_Contextual_Prompt_MT/data/iwslt_hf/"
+    model_size = "4.5B"
+    #model_checkpoint = "facebook/xglm-1.7B"
+    model_checkpoint = f"facebook/xglm-{model_size}"
+    #model_checkpoint = "facebook/xglm-7.5B"
+
+    output_dir = f"./results/xglm/{model_size}"
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+    
+    k = 3
+    prompt_talk_id =  int(1548)
+    data_files = { "test": f"{data_path}ted_en-{tgt_lang}"}
+    dataset = load_dataset("json", data_files=data_files)
+    
+    tokenizer = XGLMTokenizer.from_pretrained(model_checkpoint) # ,  truncation=True, padding='max_length', max_new_tokens=250, return_tensors="pt") # padding_side = 'left',
+    prompt = generate_prompt(dataset, tgt_lang, k, prompt_talk_id)
+    inputs = preprocess_function(tgt_lang, prompt, prompt_talk_id, tokenizer, dataset["test"]).input_ids
+    labels = preprocess_function(tgt_lang, prompt, prompt_talk_id, tokenizer, dataset["test"]).labels
+    model = XGLMForCausalLM.from_pretrained(model_checkpoint)
+    
+    # Predict
+    
+    outputs = model.generate(inputs, max_new_tokens=100, do_sample=True, top_k=50, top_p=0.95) # 
+    predictions = tokenizer.batch_decode(outputs, skip_special_tokens=True)
+    print ("predictions", predictions)
+    
+    # Evaluate 
+    eval_preds = (outputs, labels, inputs)
+    print (eval_preds)
+    compute_metrics(dataset, output_dir, tgt_lang, tokenizer, eval_preds)
+    
+    
+    
+    """
+    decoded_predictions = []
+    for ip, label in zip (inputs, labels):
+        print ("check inputs shape", ip)
+        output = model.generate(ip, max_new_tokens=100, do_sample=True, top_k=50, top_p=0.95) # 
+        decoded_pred = tokenizer.batch_decode(output, skip_special_tokens=True)
+        decoded_predictions.append(decoded_pred)
+      
+    print ("predictions", predictions)
+    """
+
+
+    """
     # Parameters
     tgt_lang = "ja"
     data_path = "/home/sumire/thesis/LLM_Contextual_Prompt_MT/data/iwslt_hf/"
     output_dir = "./results/playingaround"
-    #model_checkpoint = "facebook/xglm-1.7B"
+    model_checkpoint = "facebook/xglm-1.7B"
     #model_checkpoint = "facebook/xglm-2.9B"
     #model_checkpoint = "facebook/xglm-4.5B"
-    model_checkpoint = "facebook/xglm-7.5B"
+    #model_checkpoint = "facebook/xglm-7.5B"
     #model_checkpoint = "bigscience/mt0-small"
     k = 3
     prompt_talk_id =  int(1548)
@@ -56,15 +106,15 @@ def main():
     dataset = load_dataset("json", data_files=data_files)
     #print ("Primt dataset"dataset["test"][1]["doc"]["en"])
     prompt = generate_prompt(dataset, tgt_lang, k, prompt_talk_id)
-    """
+    
     tokenized_datasets = dataset.map(
         partial(preprocess_function, tgt_lang, prompt, prompt_talk_id, tokenizer),
         batched=True,
         remove_columns=dataset["test"].column_names,
         )
     tokenized_datasets.save_to_disk(f'./tokenized/ted_en-{tgt_lang}')
-    """
-    tokenized_datasets = load_from_disk(f'./tokenized/ted_en-{tgt_lang}')
+    
+    #tokenized_datasets = load_from_disk(f'./tokenized/ted_en-{tgt_lang}')
 
     data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)
 
@@ -78,6 +128,7 @@ def main():
         )
     model.eval()
     trainer.evaluate()
+    """
     
 
 if __name__ == "__main__":
