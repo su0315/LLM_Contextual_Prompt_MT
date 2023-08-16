5f899ec5d09e2d6e648866e2a26de693464b235a
Sumire Honda <pu.sumirehonda@gmail.com>
2023-08-16 22:12:27 +0000

    art: context concatenation

--- main/preprocess.py
+++ main/preprocess.py
@@ -5,9 +5,9 @@ def generate_context(src_context_size, prompt_talk_id, data):
     prompt_talk_index = data["talk_id"].index(prompt_talk_id)
 
     for doc_idx, doc in enumerate(data["doc"]):
-        doc_input = [sent for doc in data["doc"][prompt_talk_index+1:3] for sent in doc["en"][:10]] # 10 sentences from each document
+        doc_input = [sent for doc in data["doc"][prompt_talk_index+1:3] for sent in doc["en"]] # 10 sentences from each document
         for idx, ip in enumerate(doc_input):
-            _context = "Given context: \n\n "
+            _context = "Given context:\n\n"
 
             # Check each context index given the context size and current input index
             for context_window in range(src_context_size, 0, -1):
@@ -42,10 +42,10 @@ def generate_prompt(data, tgt_lang, model_checkpoint, k, prompt_talk_id):
         for doc in data:
             if doc["talk_id"] == prompt_talk_id:
                 for en_sent, tgt_lang_sent in zip(doc["doc"]["en"][13:13+k], doc["doc"][tgt_lang][13:13+k]):
-                    k_shot =f"{en_sent} => {tgt_lang_sent} \n "
+                    k_shot =f"{en_sent} => {tgt_lang_sent}\n"
                     _prompt += k_shot
                 break
-        _prompt = f"""Translate English to {target_language[tgt_lang]}: \n\n {_prompt}"""
+        _prompt = f"""Translate English to {target_language[tgt_lang]}:\n\n{_prompt}"""
             
     print("PROMPT", _prompt)
     return _prompt
@@ -54,8 +54,9 @@ def generate_prompt(data, tgt_lang, model_checkpoint, k, prompt_talk_id):
 def preprocess_function(src_context_size, tgt_lang, model_checkpoint, prompt, prompt_talk_id, max_length, tokenizer, data): # data should be splitted into train / dev / test internally
     
     # Identify prompt talk and eliminate it from the inputs
-    prompt_talk_index = data["talk_id"].index(prompt_talk_id)
+    #prompt_talk_index = data["talk_id"].index(prompt_talk_id)
     inputs = []
+    
 
     if "xglm" in model_checkpoint:
         after_ip = " = "
@@ -67,24 +68,23 @@ def preprocess_function(src_context_size, tgt_lang, model_checkpoint, prompt, pr
     if src_context_size >= 1:
         print ("Yes2")
         for doc_idx, doc in enumerate(data["doc"]):
-            doc_input = [sent for doc in data["doc"][prompt_talk_index+1:] for sent in doc["en"]] # 10 sentences from each document
-            
+            doc_input = [sent for sent in doc["en"]] # 10 sentences from each document
+
             for idx, ip in enumerate(doc_input):
-                _context = "Given context: \n\n "
+                _context = "Given context:\n\n"
 
                 # Check each context index given the context size and current input index
                 for context_window in range(src_context_size, 0, -1):
                     context_idx = idx - context_window
                     # If context idx is not the left side of the beggining of the doc_inputs
                     if context_idx >= 0: 
-                        _context += doc_input[context_idx] + " \n "
+                        _context += doc_input[context_idx] + "\n"
 
                 concat_input = _context + "\n" + prompt + ip + after_ip
                 inputs.append(concat_input)
 
     else:
-        inputs = [prompt + ip + after_ip for doc in data["doc"][prompt_talk_index+1:] for sent in doc["en"]] 
-
+        inputs = [prompt + ip + after_ip for doc in data["doc"][:] for sent in doc["en"]] 
 
         #if "xglm" in model_checkpoint:
             #doc_input = [_context + prompt + ip + ' = ' for doc in data["doc"][prompt_talk_index+1:] for sent in doc["en"]] ## [1:] to eliminate Few shot example
@@ -100,9 +100,13 @@ def preprocess_function(src_context_size, tgt_lang, model_checkpoint, prompt, pr
     #elif "llama" in model_checkpoint:
         #inputs = [f"""Translate English to {target_language[tgt_lang]}: \n\n {prompt} {sent} =>""" for doc in data["doc"][prompt_talk_index+1:] for sent in doc["en"]] ## [1:] to eliminate Few shot example
     
-    targets = [sent for doc in data["doc"][prompt_talk_index+1:]for sent in doc[tgt_lang]]
-    print ("Yes3")
+
+    targets = [sent for doc in data["doc"] for sent in doc[tgt_lang]]
     
+    print ("INPUT EXAMPLE 0")
+    print (inputs[0])
+    print ("INPUT EXAMPLE 1")
+    print (inputs[1])
     model_inputs = tokenizer(
         inputs, text_target=targets, return_tensors="pt", max_length=max_length, padding='max_length', truncation=True) #, max_length=500, truncation=True, padding='max_length', return_tensors="pt"
     
