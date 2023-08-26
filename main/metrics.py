48b89e121fe15179df46cb869a704fd44c1269d0
Sumire Honda <pu.sumirehonda@gmail.com>
2023-08-26 20:45:26 +0000

    xglm input postprocess + ko/zh sacrebleu

--- main/metrics.py
+++ main/metrics.py
@@ -4,6 +4,7 @@ import preprocess
 import json
 
 def postprocess_text(preds, labels, input_ids, model_checkpoint):
+
     preds = [pred.strip() for pred in preds]
     labels = [[label.strip()] for label in labels]
     input_ids = [input_id.strip() for input_id in input_ids]
@@ -12,6 +13,10 @@ def postprocess_text(preds, labels, input_ids, model_checkpoint):
     if "llama" in model_checkpoint:
         preds = [pred.split("\n")[0] for pred in preds] # Extract only the first prediction 
         input_ids = [input_id.split("\n")[-1][:-2] for input_id in input_ids] # Extract the input from examples + input [:-2] works for removing "=>" ?
+        
+    if "xglm" in model_checkpoint:
+        input_ids = [input_id[:-1] for input_id in input_ids] # Extract the input from examples + input [:-1] works for removing "=" ?
+        print ("decoded IP", input_ids)
 
     return preds, labels, input_ids
 
@@ -28,14 +33,12 @@ def compute_metrics(dataset, model_checkpoint, output_dir, tgt_lang, tokenizer, 
     
     decoded_preds = tokenizer.batch_decode(preds, skip_special_tokens=True)
     
-    # Labels
-    decoded_labels = np.where(decoded_labels != -100, decoded_labels, tokenizer.pad_token_id)
-    
     # Input_ids
     input_ids = np.where(input_ids != -100, input_ids, tokenizer.pad_token_id)
     
     if "xglm" in model_checkpoint:
-        input_ids = [ np.array_split(item, np.where(item == sep)[-1])[-1][:-1]  for item in input_ids ]
+        input_ids = [ np.array_split(item, np.where(item == sep)[-1])[-1] for item in input_ids ] 
+    
     decoded_input_ids = tokenizer.batch_decode(input_ids, skip_special_tokens=True)
    
     decoded_preds, decoded_labels, decoded_input_ids = postprocess_text(decoded_preds, decoded_labels, decoded_input_ids,  model_checkpoint)
@@ -47,6 +50,10 @@ def compute_metrics(dataset, model_checkpoint, output_dir, tgt_lang, tokenizer, 
     # bleu
     if tgt_lang == "ja":
         bleu = metric1.compute(predictions=decoded_preds, references=decoded_labels, tokenize='ja-mecab')
+    elif tgt_lang == "ko":
+        bleu = metric1.compute(predictions=decoded_preds, references=decoded_labels, tokenize='ko-mecab')
+    elif tgt_lang == "zh":
+        bleu = metric1.compute(predictions=decoded_preds, references=decoded_labels, tokenize='zh')
     else: 
         bleu = metric1.compute(predictions=decoded_preds, references=decoded_labels)
     result = {"bleu": bleu["score"]}
