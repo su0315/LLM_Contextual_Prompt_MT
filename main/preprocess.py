817e06d7b96fd6a3514b523366da214036235329
Sumire Honda <pu.sumirehonda@gmail.com>
2023-09-03 19:54:37 +0000

    pos: randomness in prompt choice

--- main/preprocess.py
+++ main/preprocess.py
@@ -1,32 +1,58 @@
+import random
+
 def generate_prompt(data, tgt_lang, model_checkpoint, k, prompt_talk_id):
 
     target_language = {"ja": "Japanese", "de":"German", "fr":"French", "ko": "Korean", "ar": "Arabic", "zh":"Chinese"}
 
     #K-shot Prompt
-    _prompt = ""
     
     if "xglm" in model_checkpoint:
-        for doc in data:
-            if doc["talk_id"] == prompt_talk_id:
-                for en_sent, tgt_lang_sent in zip(doc["doc"]["en"][13:13+k], doc["doc"][tgt_lang][13:13+k]):
-                    k_shot = f"{en_sent} = {tgt_lang_sent} </s> "
-                    _prompt += k_shot
-                break
+        after_ip = " = "
+        sep_token = "</s>"
+        _prompt = ""
 
-    elif "llama" or "Llama" in model_checkpoint: 
-        for doc in data:
-            if doc["talk_id"] == prompt_talk_id:
-                print ("Prompt found in trainset")
-                for en_sent, tgt_lang_sent in zip(doc["doc"]["en"][13:13+k], doc["doc"][tgt_lang][13:13+k]):
-                    k_shot =f"{en_sent} => {tgt_lang_sent}\n"
-                    _prompt += k_shot
-                break
-            else:
-                print ("No prompt found")
+    elif "llama" or "Llama" in model_checkpoint:
+        after_ip = " => "
+        sep_token = "\n"
+        _prompt = f"""Translate English to {target_language[tgt_lang]}:{sep_token}{sep_token}"""
+
+    random.seed(10)
+    # Random prompt doc id
+    for i in range(k):
+        rd_doc_id = random.randint(0, len(data["talk_id"]))
+        print ("rd_doc_id", rd_doc_id)
+        talk_id = data["talk_id"][rd_doc_id]
+        print ("talk_id", talk_id)
+        rd_sent_id =random.randint(0, len(data["doc"][rd_doc_id]))
+        print ("rd_sent_id", rd_sent_id)
+        en_sent = data["doc"][rd_doc_id]["en"][rd_sent_id]
+        tgt_lang_sent =  data["doc"][rd_doc_id][tgt_lang][rd_sent_id]
+        k_shot = en_sent + after_ip + tgt_lang_sent + sep_token
+        _prompt += k_shot
     
+    # 
+    # if "xglm" in model_checkpoint:
+    #     for doc in data:
+    #         if doc["talk_id"] == prompt_talk_id:
+    #             for en_sent, tgt_lang_sent in zip(doc["doc"]["en"][13:13+k], doc["doc"][tgt_lang][13:13+k]):
+    #                 k_shot = f"{en_sent} = {tgt_lang_sent} </s> "
+    #                 _prompt += k_shot
+    #             break
+
+    # elif "llama" or "Llama" in model_checkpoint: 
+    #     for doc in data:
+    #         if doc["talk_id"] == prompt_talk_id:
+    #             print ("Prompt found in trainset")
+    #             for en_sent, tgt_lang_sent in zip(doc["doc"]["en"][13:13+k], doc["doc"][tgt_lang][13:13+k]):
+    #                 k_shot =f"{en_sent} => {tgt_lang_sent}\n"
+    #                 _prompt += k_shot
+    #             break
+    #         else:
+    #             print ("No prompt found")
     
-        _prompt = f"""Translate English to {target_language[tgt_lang]}:\n\n{_prompt}"""
-            
+    #     _prompt = f"""Translate English to {target_language[tgt_lang]}:\n\n{_prompt}"""
+    # 
+
     return _prompt
 
 
