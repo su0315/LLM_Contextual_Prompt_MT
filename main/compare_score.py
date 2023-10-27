fce7cd008afc4c9451f2a5ca01d6b8b6091087f2
Sumire Honda <pu.sumirehonda@gmail.com>
2023-10-27 19:32:26 +0000

    pos: score_comparison with custom metrics

--- main/compare_score.py
+++ main/compare_score.py
@@ -12,6 +12,7 @@ from main.preprocess import preprocess_function, generate_few_shots, preprocess_
 from main.metrics import compute_metrics
 from jsonargparse import (ActionConfigFile, ArgumentParser, Namespace,
                           namespace_to_dict)
+import argparse                         
 from tqdm import tqdm
 from eval_only import read_config, select_instances, initialize_tokenizer
 
@@ -24,14 +25,18 @@ def read_output_dirs():
     parser = ArgumentParser(description='Read output directory of the model we evaluate.')
     parser.add_argument('--output_dir_with_c', type=str, help='The directory path with context')
     parser.add_argument('--output_dir_without_c', type=str, help='The directory path without context')
+    parser.add_argument('--metrics',  type=str, help = "Comma-separated list of strings", default= "sacrebleu,comet", required=False)
     args = parser.parse_args()
     output_dir_with_c = args.output_dir_with_c
     output_dir_without_c = args.output_dir_without_c
-    return output_dir_with_c, output_dir_without_c
+    metrics = args.metrics.split(",")
+    print ("metrics", metrics)
+    return output_dir_with_c, output_dir_without_c, metrics
 
 def compare_score(
     preds_with_c,
     preds_without_c, 
+    metrics,
     api,
     model_checkpoint, 
     tokenizer,
@@ -53,60 +58,54 @@ def compare_score(
     #comet_binaries = []
     #bleu_score_diffs = []
     #comet_score_diffs = []
-    #gen_len_score_diffs = []
 
     if api: 
-        for pred_with_c, pred_without_c, label, source in zip(preds_with_c, preds_without_c, labels, sources):
+        for pred_with_c, pred_without_c, label, source in zip(preds_with_c[:2], preds_without_c[:2], labels[:2], sources[:2]):
 
             # Evaluate
             eval_pred_with_c = ([pred_with_c], [label], [source])
             eval_pred_without_c = ([pred_without_c], [label], [source])
-            result_with_c, decoded_pred_with_c, decoded_label, decoded_input_id = compute_metrics(api, model_checkpoint, output_dir_with_c, tgt_lang, tokenizer, eval_pred_with_c, prompt_type)
-            result_without_c, decoded_pred_without_c, decoded_label, decoded_input_id = compute_metrics(api, model_checkpoint, output_dir_without_c, tgt_lang, tokenizer, eval_pred_without_c, prompt_type)
+            result_with_c, decoded_pred_with_c, decoded_label, decoded_input_id = compute_metrics(metrics, api, model_checkpoint, output_dir_with_c, tgt_lang, tokenizer, eval_pred_with_c, prompt_type)
+            result_without_c, decoded_pred_without_c, decoded_label, decoded_input_id = compute_metrics(metrics, api, model_checkpoint, output_dir_without_c, tgt_lang, tokenizer, eval_pred_without_c, prompt_type)
             
-            bleu_score_with_c = result_with_c["bleu"]
-            bleu_score_without_c = result_without_c["bleu"]
-            comet_score_with_c = result_with_c["comet"]
-            comet_score_without_c = result_without_c["comet"]
-            gen_len_score_with_c = result_with_c["gen_len"]
-            gen_len_score_without_c = result_without_c["gen_len"]
+            for metric in metrics:
+                score_with_c = result_with_c[metric]
+                score_without_c = result_without_c[metric]
+                
+                score_diff = np.round(score_with_c - score_without_c, 4)
+                if score_diff > 0:
+                    binary = 1 
+                else:
+                    binary = 0
+                """
+                comet_score_diff = np.round(comet_score_with_c - comet_score_without_c, 4)
+                #comet_score_diffs.append(comet_score_diff)
+                if comet_score_diff > 0:
+                    comet_binary = 1
+                else:
+                    comet_binary = 0
+                """
+            
+                with open(output_dir_with_c+f'/{metric}_binary.txt','a', encoding='utf8') as wf:
+                    #for i in bleu_score_diffs:
+                    wf.write(str(binary)+ "\n")
+                """
+                with open(output_dir_with_c+f'/{metric}_binary.txt','a', encoding='utf8') as wf:
+                    #for i in comet_score_diffs:
+                    wf.write(str(comet_binary)+ "\n")
+                """
 
-            bleu_score_diff = np.round(bleu_score_with_c - bleu_score_without_c, 4)
-            #bleu_score_diffs.append(bleu_score_diff)
-            if bleu_score_diff > 0:
-                bleu_binary = 1 
-            else:
-                bleu_binary = 0
-            comet_score_diff = np.round(comet_score_with_c - comet_score_without_c, 4)
-            #comet_score_diffs.append(comet_score_diff)
-            if comet_score_diff > 0:
-                comet_binary = 1
-            else:
-                comet_binary = 0
-            gen_len_score_diff = gen_len_score_with_c - gen_len_score_without_c
-            #gen_len_score_diffs.append(gen_len_score_diff)
-        
-            with open(output_dir_with_c+'/bleu_binary.txt','a', encoding='utf8') as wf:
-                #for i in bleu_score_diffs:
-                wf.write(str(bleu_binary)+ "\n")
-            with open(output_dir_with_c+'/comet_binary.txt','a', encoding='utf8') as wf:
-                #for i in comet_score_diffs:
-                wf.write(str(comet_binary)+ "\n")
-
-            with open(output_dir_with_c+'/bleu_score_diff.txt','a', encoding='utf8') as wf:
-                #for i in bleu_score_diffs:
-                wf.write(str(bleu_score_diff)+ "\n")
-
-            with open(output_dir_with_c+'/comet_score_diff.txt','a', encoding='utf8') as wf:
-                #for i in comet_score_diffs:
-                wf.write(str(comet_score_diff)+ "\n")
-
-            with open(output_dir_with_c+'/gen_len_score_diff.txt','a', encoding='utf8') as wf:
-                #for i in gen_len_score_diffs:
-                wf.write(str(gen_len_score_diff)+ "\n")
+                with open(output_dir_with_c+f'/{metric}_score_diff.txt','a', encoding='utf8') as wf:
+                    #for i in bleu_score_diffs:
+                    wf.write(str(score_diff)+ "\n")
+                """
+                with open(output_dir_with_c+'/comet_score_diff.txt','a', encoding='utf8') as wf:
+                    #for i in comet_score_diffs:
+                    wf.write(str(comet_score_diff)+ "\n")
+                """
 
 def main():
-    output_dir_with_c, output_dir_without_c = read_output_dirs()
+    output_dir_with_c, output_dir_without_c, metrics = read_output_dirs()
     
     tgt_lang, data_path, src_context_size_with_c, api, model_checkpoint, batch_size, k, prompt_type, max_new_tokens, max_length, cfg_name_with_c = read_config(output_dir_with_c)
     tgt_lang, data_path, src_context_size_without_c, api, model_checkpoint, batch_size, k, prompt_type, max_new_tokens, max_length, cfg_name_with_c = read_config(output_dir_without_c)
@@ -122,6 +121,7 @@ def main():
     score_differences = compare_score(
         preds_with_c,
         preds_without_c,
+        metrics,
         api,
         model_checkpoint,
         tokenizer, 
--- main/metrics.py
+++ main/metrics.py
@@ -3,6 +3,9 @@ import numpy as np
 import preprocess
 import json
 
+metric1 = evaluate.load("sacrebleu")
+metric2 = evaluate.load("comet")
+
 def postprocess_text(preds, labels, input_ids, model_checkpoint, api, prompt_type):
 
     preds = [pred.strip() for pred in preds]
@@ -49,7 +52,7 @@ def postprocess_text(preds, labels, input_ids, model_checkpoint, api, prompt_typ
     return preds, labels, input_ids
 
 
-def compute_metrics(api, model_checkpoint, output_dir, tgt_lang, tokenizer, eval_preds, prompt_type):
+def compute_metrics(metrics, api, model_checkpoint, output_dir, tgt_lang, tokenizer, eval_preds, prompt_type):
     preds, decoded_labels, input_ids = eval_preds
     decoded_input_ids = input_ids
     
@@ -69,29 +72,33 @@ def compute_metrics(api, model_checkpoint, output_dir, tgt_lang, tokenizer, eval
     
     decoded_preds, decoded_labels, decoded_input_ids = postprocess_text(decoded_preds, decoded_labels, decoded_input_ids,  model_checkpoint, api, prompt_type)
     
-    metric1 = evaluate.load("sacrebleu")
-    metric2 =  evaluate.load("comet")
 
-    print ("BLEU-preds", decoded_preds, "BLEU-references", decoded_labels)
-    # bleu
-    if tgt_lang == "ja":
-        bleu = metric1.compute(predictions=decoded_preds, references=decoded_labels, tokenize='ja-mecab')
-    elif tgt_lang == "ko":
-        bleu = metric1.compute(predictions=decoded_preds, references=decoded_labels, tokenize='ko-mecab')
-    elif tgt_lang == "zh":
-        bleu = metric1.compute(predictions=decoded_preds, references=decoded_labels, tokenize='zh')
-    elif tgt_lang == "ar":
-        bleu = metric1.compute(predictions=decoded_preds, references=decoded_labels, tokenize='flores101')
-    else: 
-        bleu = metric1.compute(predictions=decoded_preds, references=decoded_labels)
-    result = {"bleu": bleu["score"]}
+    for metric in metrics:
+        if metric == "sacrebleu":
+            print ("BLEU-preds", decoded_preds, "BLEU-references", decoded_labels)
+            # bleu
+            if tgt_lang == "ja":
+                bleu = metric1.compute(predictions=decoded_preds, references=decoded_labels, tokenize='ja-mecab')
+            elif tgt_lang == "ko":
+                bleu = metric1.compute(predictions=decoded_preds, references=decoded_labels, tokenize='ko-mecab')
+            elif tgt_lang == "zh":
+                bleu = metric1.compute(predictions=decoded_preds, references=decoded_labels, tokenize='zh')
+            elif tgt_lang == "ar":
+                bleu = metric1.compute(predictions=decoded_preds, references=decoded_labels, tokenize='flores101')
+            else: 
+                bleu = metric1.compute(predictions=decoded_preds, references=decoded_labels)
+            result = {metric: bleu["score"]}
 
-    # comet    
-    comet = metric2.compute(predictions=decoded_preds, references=[item for decoded_label in decoded_labels for item in decoded_label], sources = decoded_input_ids)
-    result["comet"] =  np.mean(comet["scores"])
+        if metric == "comet":
+            print ("COMET-preds", decoded_preds, "COMET-references", decoded_labels)
+            # comet    
+            comet = metric2.compute(predictions=decoded_preds, references=[item for decoded_label in decoded_labels for item in decoded_label], sources = decoded_input_ids)
+            result[metric] =  np.mean(comet["scores"])
+        
     prediction_lens = [np.count_nonzero(pred != tokenizer.pad_token_id) for pred in preds]
     result["gen_len"] = np.mean(prediction_lens)
     result = {k: round(v, 4) for k, v in result.items()}
     print(result)
+    
 
     return result, decoded_preds, decoded_labels, decoded_input_ids
