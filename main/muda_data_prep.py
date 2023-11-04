98d3e2d83de770e4c6cdd9dd1e2c81a549ec9bf0
Sumire Honda <pu.sumirehonda@gmail.com>
2023-11-04 09:50:29 +0000

    pos: muda dataprep

--- main/muda_data_prep.py
+++ main/muda_data_prep.py
@@ -1,9 +1,10 @@
-from datasets import load_dataset, concatenate_datasets, load_from_disk
+from datasets import load_dataset, concatenate_datasets
+import os 
 
 def mudadata_for_ted(data_path, tgt_lang, output_dir):
-    data_files = { "train": f"{data_path}train_ted_en-{tgt_lang}",  "test": f"{data_path}test_ted_en-{tgt_lang}"}
+    data_files = { "train": f"{data_path}train_ted_en-{tgt_lang}", "val": f"{data_path}val_ted_en-{tgt_lang}",  "test": f"{data_path}test_ted_en-{tgt_lang}"}
+    print (f"{data_path}train_ted_en-{tgt_lang}")
     dataset = load_dataset("json", data_files=data_files)
-
     data = dataset["test"]
 
     doc_ids = []
@@ -15,19 +16,21 @@ def mudadata_for_ted(data_path, tgt_lang, output_dir):
     src = [sent for doc in data["doc"] for sent in doc["en"]]
     tgt = [sent for doc in data["doc"] for sent in doc[tgt_lang]]
 
-    with open(output_dir+f'ted/ted_test_src.en','w', encoding='utf8') as wf:
+    with open(output_dir+f'ted_test_src.en','w', encoding='utf8') as wf:
         for item in src:
             wf.write(item+ '\n')
 
-    with open(output_dir+f'ted/ted_test_tgt.{tgt_lang}','w', encoding='utf8') as wf:
+    with open(output_dir+f'ted_test_tgt.{tgt_lang}','w', encoding='utf8') as wf:
         for item in tgt:
             wf.write(item+ '\n')
 
-    with open(output_dir+f'ted/ted_test.docids','w', encoding='utf8') as wf:
+    with open(output_dir+f'ted_test.docids','w', encoding='utf8') as wf:
         for item in doc_ids:
             print (item)
             wf.write(f"{item}\n")
 
 ted_data_path = "/home/sumire/thesis/LLM_Contextual_Prompt_MT/data/iwslt_hf/"
-output_dir = "/home/sumire/thesis/LLM_Contextual_Prompt_MT/data/muda_tagged/"
+output_dir = "/home/sumire/thesis/LLM_Contextual_Prompt_MT/data/muda_tagged/ted/"
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
 mudadata_for_ted(data_path=ted_data_path, tgt_lang="ja", output_dir=output_dir)
